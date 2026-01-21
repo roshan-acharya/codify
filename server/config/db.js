e08861e2ab638bc81c42ac03d0cbfbd59e2c9d38
Roshan Acharya <85246971+roshan-acharya@users.noreply.github.com>
2026-01-21 16:40:30 +0545

    Update database connection URL format

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 export default function connectDb() {
   //connection url
   const connection_url =
-    "mongodb+srv://roshanmern:92h1wpz1ONf6rJMC@cluster0.acjdibm.mongodb.net/noteify?retryWrites=true&w=majority";
+    // "mongodb+srv://roshanmern:92h1wpz1ONf6rJMC@cluster0.acjdibm.mongodb.net/noteify?retryWrites=true&w=majority";
   //connecting to db
   mongoose.connect(connection_url);
   try {
